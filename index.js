492d4dd14d5fb730424efffaa76d1c0c5aa3823f
Sumanta Kumar Adhek ~~ WoosWeb <88125165+SumantaGitWeb@users.noreply.github.com>
2022-01-11 10:02:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,83 @@
+function data(){
+    return{
+        inp : "",
+        addQ(){
+            this.inp += "Q"
+        },
+        addW(){
+            this.inp += "W"
+        },
+        addR(){
+            this.inp += "R"
+        },
+        addT(){
+            this.inp += "T"
+        },
+        addY(){
+            this.inp += "Y"
+        },
+        addU(){
+            this.inp += "U"
+        },
+        addI(){
+            this.inp += "I"
+        },
+        addO(){
+            this.inp += "O"
+        },
+        addP(){
+            this.inp += "P"
+        },
+        addA(){
+            this.inp += "A"
+        },
+        addS(){
+            this.inp += "S"
+        },
+        addD(){
+            this.inp += "D"
+        },
+        addF(){
+            this.inp += "F"
+        },
+        addG(){
+            this.inp += "G"
+        },
+        addH(){
+            this.inp += "H"
+        },
+        addJ(){
+            this.inp += "J"
+        },
+        addK(){
+            this.inp += "K"
+        },
+        addL(){
+            this.inp += "L"
+        },
+        addZ(){
+            this.inp += "Z"
+        },
+        addX(){
+            this.inp += "X"
+        },
+        addC(){
+            this.inp += "C"
+        },
+        addV(){
+            this.inp += "V"
+        },
+        addB(){
+            this.inp += "B"
+        },
+        addN(){
+            this.inp += "N"
+        },
+        addM(){
+            this.inp += "M"
+        },
+        addE(){
+            this.inp += "E"
+        },
+    }
+}
